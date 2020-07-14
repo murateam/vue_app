77719572023dc88aeeca546318ea42dd1db6cfda
amurashko24 <amurashko24@gmail.com>
2020-07-14 16:45:47 +0300

    translated some lines for description

--- src/components/myPage/main.vue
+++ src/components/myPage/main.vue
@@ -16,34 +16,24 @@
               <p>
                 My name is Andrei and i'm new in the web development.
               </p>
-              <p>
+              <!-- <p>
                 Programming has always seemed to me a difficult job but very interesting.
                 I didn't have enough confidence that i will succeed in programming.
-              </p>
+              </p> -->
               <p>
-                Меня всегда тянуло в разработку. Но так как мне казалось что программирование
-                это сложно, решил начать с чего нибудь по проще, с 3Д.
-                По моему мнения 3Д проще понять.
-                Во время работы мне приходилось сталкиваться с программированием.
-                Например в игровом движке Unreal Engine
-                есть возможность без знаний языка C++
-                собирать какую то логику различного назначения на blueprints.
-                Чем глубже я погружался в 3д, тем чаще я сталкивался
-                с различными дополнительными
-                инструментами написанными энтузиастами или профессионалами
-                для таких программ как autodesk maya или houdini.
-                Тем более создание своих инструментов, а не использоание созданных
-                до тебя казалось мне чем то магическим.
-                И в один момент мне в голову пришла мысль что в настоящее время
-                без знаний программирования не обойтись.
-                Оставаясь в 3д индустрии или любой другой программной разработке.
-                Знание программирования в целом увеличивает понимание всего процесса в целом.
-                На основании этого я принял решения пройти обучения языку python.
-                Но так как просто изучение языка не имеет смысла, а для отработки своих
-                знания нужно какое то прикладное назначение языка
-                Мне на глаза попался курс Веб разработки полного цикла на python.
-                Этот курс координально изменил мое отношение к программированию.
-                Я влюбился в программирование. Я закончил курс.
+                I have always wanted to be in software development.
+                But it seemed to me that programming it's difficult.
+                It is, but i thought that i can't handle it.
+                I began my path into development industry since 3D (gameDev)
+                It seemed to me that 3D is more easily to understand then programming.
+                During the work in 3D i faced several times with programming.
+                For example the game engine "Unreal Engine" give us a possibility
+                without knowledge in C++ implement some logic in depending on requireds
+                with the help of "blueprints".
+                As deeper i sank into 3d as often I faced with programming.
+                I've faced with different tools written by enthusiasts or professionals
+                for programs like Houdini or Autodesk Maya written in Python language.
+                At one point in time 
               </p>
               <p>
                 This web app is a result of my learning on a Full Stack Web Developer cource.
